1d93a8c6c7314c4d61771f1250311b477b6269be
DafyddEvans2813865 <156840512+DafyddEvans2813865@users.noreply.github.com>
2025-10-25 14:17:07 +0100

    Scrape gets titles and hrefs

--- scraper-python.py
+++ scraper-python.py
@@ -1,11 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
 
+## returns a dict of event titles and hrefs
+
+##TODO:Allow users to update the URL to scrape different pages if needed 
+
 def scrape_website(url):
     try:
         response = requests.get(url)
         response.raise_for_status()  # Check for HTTP errors
-        print("goat")
+        soup = BeautifulSoup(response.text, 'html.parser')
+        titles_raw = soup.find_all('h2')
+        titles = [title.get_text(strip=True) for title in titles_raw]
+        hrefs = [title.find('a')['href'] for title in titles_raw if title.find('a')] 
+        events = dict(zip(titles, hrefs))
+        
+        print(events)
+
     except requests.exceptions.RequestException as e:
         print(f"Error fetching the URL: {e}") 
         return
